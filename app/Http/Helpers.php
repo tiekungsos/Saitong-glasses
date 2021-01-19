d5544a665d368eddb44d150f4f66eee82ddb3729
Thanapon Pruektan <beeze001@gmail.com>
2021-01-19 21:35:56 +0700

    commit

--- app/Http/Helpers.php
+++ app/Http/Helpers.php
@@ -7,6 +7,9 @@ use App\Models\Order;
 use App\Models\Wishlist;
 use App\Models\Shipping;
 use App\Models\Cart;
+
+namespace App\Http;
+
 // use Auth;
 class Helper{
     public static function messageList()
