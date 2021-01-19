d29cbb935dba5b1216a0a8bdd322ee85e00bb891
Thanapon Pruektan <beeze001@gmail.com>
2021-01-19 21:34:52 +0700

    commit

--- config/app.php
+++ config/app.php
@@ -236,8 +236,7 @@ return [
         'Image' => Intervention\Image\Facades\Image::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'PayPal' => Srmklive\PayPal\Facades\PayPal::class,
-        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        'Helper' => App\Http\Helper::class,
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class
     ],
 
 ];
